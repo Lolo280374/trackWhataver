a06ec31d80f856fb5159706ddf8f9d205802ed9c
lolodotzip <56828143+Lolo280374@users.noreply.github.com>
2025-10-26 00:28:12 +0200

    added handling for the actual ID (invalid = no work, otherwise works) and added a tbd info page

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const search = document.querySelector('.mainsearch-form');
+const searchInput = document.querySelector('.mainsearch-input');
+const invalidMessage = document.getElementById('invalid');
+const trackingInfo = document.getElementById('tracking-info')
+
 lucide.createIcons();
+
+search.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const query = searchInput.value.trim();
+    if (query.toLowerCase() === 'invalid') {
+        invalidMessage.style.display = 'flex';
+        document.body.classList.remove('tracking-ui')
+    } else if (query) {
+        invalidMessage.style.display = 'none';
+        document.body.classList.add('tracking-ui');
+    } else {
+        invalidMessage.style.display = 'none';
+        document.body.classList.remove('tracking-ui');
+    }
+});
